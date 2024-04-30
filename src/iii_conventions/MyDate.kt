e399fed6a88638e88e6fcb67f7d75dcf5c791701
Daniel Gimenes <danielcgimenes@gmail.com>
2024-04-30 15:09:59 -0700

    Solved exercises 25 and 26

--- src/iii_conventions/MyDate.kt
+++ src/iii_conventions/MyDate.kt
@@ -1,6 +1,12 @@
 package iii_conventions
 
-data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int)
+data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int): Comparable<MyDate> {
+    override fun compareTo(other: MyDate): Int {
+        return this.lexiOrderedStr().compareTo(other.lexiOrderedStr())
+    }
+
+    private fun lexiOrderedStr() = "%4d%2d%2d".format(year, month, dayOfMonth)
+}
 
 operator fun MyDate.rangeTo(other: MyDate): DateRange = todoTask27()
 
@@ -10,4 +16,8 @@ enum class TimeInterval {
     YEAR
 }
 
-class DateRange(val start: MyDate, val endInclusive: MyDate)
+class DateRange(override val start: MyDate, override val endInclusive: MyDate): ClosedRange<MyDate> {
+    override fun contains(value: MyDate): Boolean {
+        return value >= start && value <= endInclusive
+    }
+}
