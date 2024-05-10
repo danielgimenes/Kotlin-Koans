0fac178df72fd4957493f8fbd3d4c5a204f29e40
Daniel Gimenes <danielcgimenes@gmail.com>
2024-05-10 16:20:31 -0700

    Solved exercise 41

--- src/vi_generics/n41GenericFunctions.kt
+++ src/vi_generics/n41GenericFunctions.kt
@@ -19,12 +19,22 @@ fun task41(): Nothing = TODO(
         }
 )
 
+fun <T, C: MutableCollection<in T>> Iterable<T>.partitionTo(collectionOne: C, collectionTwo: C,
+                                                            predicateFn: (element: T) -> Boolean): Pair<C, C> {
+    forEach {
+        if (predicateFn(it)) {
+            collectionOne.add(it)
+        } else {
+            collectionTwo.add(it)
+        }
+    }
+    return collectionOne to collectionTwo
+}
+
 fun List<String>.partitionWordsAndLines(): Pair<List<String>, List<String>> {
-    task41()
-//    return partitionTo(ArrayList<String>(), ArrayList()) { s -> !s.contains(" ") }
+    return partitionTo(ArrayList<String>(), ArrayList()) { s -> !s.contains(" ") }
 }
 
 fun Set<Char>.partitionLettersAndOtherSymbols(): Pair<Set<Char>, Set<Char>> {
-    task41()
-//    return partitionTo(HashSet<Char>(), HashSet()) { c -> c in 'a'..'z' || c in 'A'..'Z'}
+    return partitionTo(HashSet<Char>(), HashSet()) { c -> c in 'a'..'z' || c in 'A'..'Z'}
 }
